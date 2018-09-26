1413da5fe291f9cbaf429da458add42d7f990efa
FengodChen <a1176553293@outlook.com>
2018-09-26 22:07:16 +0800

    Change the struction

--- include/matGC.cc
+++ include/matGC.cc
@@ -1,18 +1,66 @@
 #include "matGC.hpp"
 #include <iostream>
 
-int** getMat(int num){
-    int **m = new int*[num];
-    for(int i = 0; i < num; i++)
-        m[i] = new int[num];
-    
-    return m;
+Vec_dat::Vec_dat(int num_){
+    vec = new int[num_];
+    num = num_;
 }
 
-void inputMat(int **mat, int num){
+Vec_dat::~Vec_dat(){
+    delete vec;
+}
+
+void Vec_dat::inputVec(){
     for(int i = 0; i < num; i++)
-        for(int j = 0; j < num; j++)
-            std::cin >> mat[i][j];
-    
+        std::cin >> vec[i];
     return;
+}
+
+void Vec_dat::calMax(){
+    int *left_list = new int[num];
+    int *right_list = new int[num];
+
+    /*          The methon of getting min sub_vec:
+     * The set the vector summer is all_sum, cut the vector in
+     * vec[start_ptr] and vec[end_ptr], and the sub_vec summer
+     * is:
+     * 
+     * sub_vec_sum = all_sum - (vec[0:start_ptr-1]+vec[end_ptr+1:num-1])
+     * 
+     * From the function, we can easy see that the all_sum is const, and
+     * when we get the min of (vec[0:start_ptr-1]+vec[end_ptr+1:num-1]),
+     * we can get the max of sub_vec_sum.
+     */
+
+    int sum = 0;
+    for(int i = 0; i < num; i++){
+        left_list[i] = sum;
+        sum += vec[i];
+    }
+
+    sum = 0;
+    for(int i = num-1; i >= 0; i--){
+        right_list[i] = sum;
+        sum += vec[i];
+    }
+
+    int min = left_list[0] + right_list[num-1];
+    start_ptr = 0, end_ptr = num-1;
+    for(int s = 0; s < num; s++){
+        for(int e = num-1; e >= s; e--){
+            if(left_list[s]+right_list[e] < min){
+                min = left_list[s] + right_list[e];
+                start_ptr = s;
+                end_ptr = e;
+            }
+        }
+    }
+
+    sum = 0;
+    for(int i = start_ptr; i <= end_ptr; i++)
+        sum += vec[i];
+    
+    max_sum = sum;
+
+    delete left_list, right_list;
 }
--- include/matGC.hpp
+++ include/matGC.hpp
@@ -1,10 +1,22 @@
 #ifndef MAT_GC_HPP_
 #define MAT_GC_HPP_
 
-// Get num*num Matrix and return it
-int** getMat(int num);
+class Vec_dat{
+    // vec[num]
+    int * vec;                 
+    int num;
+    // Max sub_vector in father_vector
+    // from vec[start_ptr] to vec[end_ptr].
+    int start_ptr, end_ptr;
+    int max_sum;
 
-// Input data to Matrix
-void inputMat(int **mat, int num);
+    // Get Vector.
+    Vec_dat(int num_);
+    ~Vec_dat();
+    // Input data to Vector.
+    void inputVec();
+    // Calculate Max sum and two ptr
+    void calMax();
+};
 
 #endif
