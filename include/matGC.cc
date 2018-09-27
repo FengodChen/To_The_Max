7fa981832c29fd21d23d33ad5d877d3f2b764ae7
FengodChen <a1176553293@outlook.com>
2018-09-27 13:39:32 +0800

    finish most struction method

--- include/matGC.cc
+++ include/matGC.cc
@@ -1,13 +1,18 @@
 #include "matGC.hpp"
 #include <iostream>
+#include <cmath>
 
 Vec_dat::Vec_dat(int num_){
     vec = new int[num_];
+    left_list = new int[num_];
+    right_list = new int[num_];
     num = num_;
 }
 
 Vec_dat::~Vec_dat(){
     delete vec;
+    delete left_list;
+    delete right_list;
 }
 
 void Vec_dat::inputVec(){
@@ -18,9 +23,6 @@ void Vec_dat::inputVec(){
 }
 
 void Vec_dat::calMax(){
-    int *left_list = new int[num];
-    int *right_list = new int[num];
-
     /*          The methon of getting min sub_vec:
      * The set the vector summer is all_sum, cut the vector in
      * vec[start_ptr] and vec[end_ptr], and the sub_vec summer
@@ -110,7 +112,44 @@ void Vec_dat::setVec(int *vc){
     return;
 }
 
-Vec_dat Vec_dat::operator+(Vec_dat vd){
-    Vec_dat new_vd(num);
-    //TODO
+Vec_dat operator+(Vec_dat vd_1, Vec_dat vd_2){
+    Vec_dat vd_new(vd_1.num);
+
+    for(int i = 0; i < vd_new.num; i++){
+        vd_new.vec[i] = vd_1.vec[i] + vd_2.vec[i];
+        vd_new.left_list[i] = vd_1.left_list[i] + vd_2.left_list[i];
+        vd_new.right_list[i] = vd_1.right_list[i] + vd_2.right_list[i];
+    }
+
+    if(vd_1.start_ptr==vd_2.start_ptr && vd_1.end_ptr==vd_2.end_ptr){
+        vd_new.max_sum = vd_1.max_sum + vd_2.max_sum;
+        vd_new.start_ptr = vd_1.start_ptr;
+        vd_new.end_ptr = vd_1.end_ptr;
+    }else if(vd_1.start_ptr == vd_2.start_ptr){
+        vd_new.start_ptr = vd_1.start_ptr;
+        int bigPtr = std::max(vd_1.end_ptr, vd_2.end_ptr);
+        int smallPtr = std::min(vd_1.end_ptr, vd_2.end_ptr);
+        if(vd_new.right_list[smallPtr]-vd_new.right_list[bigPtr] >= 0){
+            vd_new.end_ptr = bigPtr;
+        }else{
+            vd_new.end_ptr = smallPtr;
+        }
+        vd_new.max_sum = 0;
+        for(int i = vd_new.start_ptr; i <= vd_new.end_ptr; i++)
+            vd_new.max_sum += vd_new.vec[i];
+    }else if(vd_1.end_ptr == vd_2.end_ptr){
+        vd_new.end_ptr = vd_1.end_ptr;
+        int bigPtr = std::max(vd_1.end_ptr, vd_2.end_ptr);
+        int smallPtr = std::min(vd_1.end_ptr, vd_2.end_ptr);
+        if(vd_new.right_list[bigPtr]-vd_new.right_list[smallPtr] >= 0){
+            vd_new.start_ptr = smallPtr;
+        }else{
+            vd_new.start_ptr = bigPtr;
+        }
+        vd_new.max_sum = 0;
+        for(int i = vd_new.start_ptr; i <= vd_new.end_ptr; i++)
+            vd_new.max_sum += vd_new.vec[i];
+    }
+
+    return vd_new;
 }
--- include/matGC.hpp
+++ include/matGC.hpp
@@ -10,6 +10,8 @@ class Vec_dat{
     // from vec[start_ptr] to vec[end_ptr].
     int start_ptr, end_ptr;
     int max_sum;
+    // 
+    int *left_list, *right_list;
 
     public:
     // Get Vector.
@@ -31,8 +33,11 @@ class Vec_dat{
     void setEndPtr(int p_);
     void setMaxSum(int sum_);
     void setVec(int *vc);
-    // Operator
-    Vec_dat operator+(Vec_dat vd);
+    // Friend function
+    friend Vec_dat operator+(Vec_dat vd_1, Vec_dat vd_2);
 };
+
+// Operator
+Vec_dat operator+(Vec_dat vd_1, Vec_dat vd_2);
 
 #endif
